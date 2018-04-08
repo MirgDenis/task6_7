98a95a80bee4919bddc270fabbd8d08ba9546248
MirgDenis <mirgdenis@gmail.com>
2018-04-08 23:44:22 +0300

    Initial commit

--- vm1.sh
+++ vm1.sh
@@ -7,7 +7,7 @@ source "$dir/vm1.config"
 echo -e "\n# Internal. Host-only\nauto $INTERNAL_IF\niface $INTERNAL_IF inet static\naddress $(echo $INT_IP | cut -d / -f 1)\nnetmask $(echo $INT_IP | cut -d / -f 2)" >> /etc/network/interfaces
 echo -e "\n# VLAN\nauto $INTERNAL_IF.$VLAN\niface $INTERNAL_IF.$VLAN inet static\naddress $(echo $VLAN_IP | cut -d / -f 1)\nnetmask $(echo $VLAN_IP | cut -d / -f 2)\nvlan-raw-device $INTERNAL_IF" >> /etc/network/interfaces
 
-#Checking DHCP or static and configure
+#Checking DHCP or static and configure External
 
 if [ "$EXT_IP" == DHCP ]
 then
@@ -26,4 +26,13 @@ ifdown $INTERNAL_IF && ifup $INTERNAL_IF
 ifdown $INTERNAL_IF.$VLAN && ifup $INTERNAL_IF.$VLAN
 ifdown $EXTERNAL_IF && ifup $EXTERNAL_IF
 #ifdown $MANAGMENT_IF && ifup $MANAGMENT_IF
+
+#Iptables for access to internet from VM2
+
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
+sysctl --system
+iptables -t nat -A POSTROUTING -o $EXTERNAL_IF -j MASQUERADE
+
+
+
 
--- vm2.sh
+++ vm2.sh
@@ -3,8 +3,7 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "$dir/vm2.config"
 #Configure VLAN and Internal interface
-
-echo -e "\n# Internal. Host-only\nauto $INTERNAL_IF\niface $INTERNAL_IF inet static\naddress $(echo $INT_IP | cut -d / -f 1)\nnetmask $(echo $INT_IP | cut -d / -f 2)" >> /etc/network/interfaces
+echo -e "\n# Internal. Host-only\nauto $INTERNAL_IF\niface $INTERNAL_IF inet static\naddress $(echo $INT_IP | cut -d / -f 1)\nnetmask $(echo $INT_IP | cut -d / -f 2)\ndns-nameservers 8.8.8.8" >> /etc/network/interfaces
 echo -e "\n# VLAN\nauto $INTERNAL_IF.$VLAN\niface $INTERNAL_IF.$VLAN inet static\naddress $(echo $VLAN_IP | cut -d / -f 1)\nnetmask $(echo $VLAN_IP | cut -d / -f 2)\nvlan-raw-device $INTERNAL_IF" >> /etc/network/interfaces
 
 #Configure Manage interface
@@ -17,4 +16,9 @@ ifdown $INTERNAL_IF && ifup $INTERNAL_IF
 ifdown $INTERNAL_IF.$VLAN && ifup $INTERNAL_IF.$VLAN
 #ifdown $MANAGMENT_IF && ifup $MANAGMENT_IF
 
+#Install and configure Apache2
+
+apt-get -y install apache2
+sed -i '/<Directory \/>/a Order Allow,Deny\nAllow from 10.0.1.1' /etc/apache2/apache2.conf
+service apache2 restart
 
