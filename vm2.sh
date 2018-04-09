6c6e99cc480f7e02ab2e1e6afaa3fbeb404fb2d9
MirgDenis <mirgdenis@gmail.com>
2018-04-09 16:12:39 +0300

    Initial commit

--- vm2.sh
+++ vm2.sh
@@ -2,9 +2,10 @@
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "$dir/vm2.config"
+source <(grep 'INT_IP=.*' $dir/vm1.config)
 #Configure VLAN and Internal interface
-echo -e "\n# Internal. Host-only\nauto $INTERNAL_IF\niface $INTERNAL_IF inet static\naddress $(echo $INT_IP | cut -d / -f 1)\nnetmask $(echo $INT_IP | cut -d / -f 2)\ndns-nameservers 8.8.8.8" >> /etc/network/interfaces
-echo -e "\n# VLAN\nauto $INTERNAL_IF.$VLAN\niface $INTERNAL_IF.$VLAN inet static\naddress $(echo $VLAN_IP | cut -d / -f 1)\nnetmask $(echo $VLAN_IP | cut -d / -f 2)\nvlan-raw-device $INTERNAL_IF" >> /etc/network/interfaces
+echo -e "\n# Internal. Host-only\nauto $INTERNAL_IF\niface $INTERNAL_IF inet static\naddress $(echo $INTERNAL_IP | cut -d / -f 1)\nnetmask $(echo $INTERNAL_IP | cut -d / -f 2)\ngateway $(echo $INT_IP | cut -d / -f 1)\ndns-nameservers 8.8.8.8" >> /etc/network/interfaces
+echo -e "\n# VLAN\nauto $INTERNAL_IF.$VLAN\niface $INTERNAL_IF.$VLAN inet static\naddress $(echo $APACHE_VLAN_IP | cut -d / -f 1)\nnetmask $(echo $APACHE_VLAN_IP | cut -d / -f 2)\nvlan-raw-device $INTERNAL_IF" >> /etc/network/interfaces
 
 #Configure Manage interface
 
@@ -18,7 +19,7 @@ ifdown $INTERNAL_IF.$VLAN && ifup $INTERNAL_IF.$VLAN
 
 #Install and configure Apache2
 
-apt-get -y install apache2
-sed -i '/<Directory \/>/a Order Allow,Deny\nAllow from 10.0.1.1' /etc/apache2/apache2.conf
-service apache2 restart
+#apt-get -y install apache2
+#sed -i '/<Directory \/>/a Order Allow,Deny\nAllow from 10.0.1.1' /etc/apache2/apache2.conf
+#service apache2 restart
 
